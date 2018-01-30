7b64d2a4a5fc48c43c2e9a347daeff7af4b2ce7c
xdxd <NPStudent@DESKTOP-5DJ839C>
2018-01-31 04:48:08 +0800

    Added Car, Insurance, Medical, Rider, Travel classes.

---
+++ AOOAD_Ass/Insurance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOOAD_Ass
+{
+    class Insurance
+    {
+        public string policyNo { get; set; }
+        public string termsCondition { get; set; }
+        public DateTime maturityDate { get; set; }
+        public List<Rider> ridersList { get; set; }
+        public float payOut { get; set; }
+    }
+}
---
+++ AOOAD_Ass/Medical.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOOAD_Ass
+{
+    class Medical:Insurance
+    {
+        public string severity { get; set; }
+    }
+}
