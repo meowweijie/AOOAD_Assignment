8e3924306996efa8b647e3d375b19d1e698b2223
Weijie <s10171408@connect.np.edu.sg>
2018-02-03 18:01:38 +0800

    Added state design pattern, sorry for the mess feel free to make it neater XD

--- AOOAD_Ass/State/Lapsed.cs
+++ AOOAD_Ass/State/Lapsed.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AOOAD_Ass.Policies;
 
 namespace AOOAD_Ass.State
 {
     class Lapsed : PolicyState
     {
+        private Policy policy;
+
+        public Lapsed(Policy policy)
+        {
+            this.policy = policy;
+        }
+        public void activatePolicy()
+        {
+            policy.setState(policy.getActiveState());
+            Console.WriteLine("Successfully changed from lapsed to active state!");
+        }
+
+        public void archivePolicy()
+        {
+            Console.WriteLine("Cannot archive a lapsed policy!");
+        }
+
+        public void lapsePolicy()
+        {
+            Console.WriteLine("Cannot change back to the same state..");
+        }
+
+        public void terminatePolicy(bool penalty)
+        {
+            if (penalty)
+                Console.WriteLine("Customer terminated this, a penalty fee must be charged");
+            else
+                Console.WriteLine("Agent terminated this, no penalty is charged");
+            policy.setState(policy.getTerminatedState());
+        }
     }
 }
--- AOOAD_Ass/State/PolicyState.cs
+++ AOOAD_Ass/State/PolicyState.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AOOAD_Ass.Policies;
 
 namespace AOOAD_Ass.State
 {
     interface PolicyState
     {
-
+        void lapsePolicy();
+        void activatePolicy();
+        void terminatePolicy(bool penalty);
+        void archivePolicy();
     }
 }
